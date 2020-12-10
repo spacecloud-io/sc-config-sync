63ff552278bf84c268ea7dc55bb27cc5d9b8d9c1
sharadregoti <sharad@spaceuptech.com>
2020-12-10 10:10:14 +0530

    Following changes were made 1) For apply spec object, we are now using the new batch apply endpoint of space cloud 2) Added github actions

--- utils/database_getters.go
+++ utils/database_getters.go
@@ -1,12 +1,9 @@
 package utils
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/spaceuptech/helpers"
 
@@ -62,68 +59,10 @@ func SyncDatabase(ctx context.Context, token string, req *model.DBSyncRequest) e
 	}
 	helpers.Logger.LogInfo(helpers.GetRequestID(ctx), fmt.Sprintf("Successfully fetched database prepared query, total count (%d)))", len(specObjs)), nil)
 
-	for _, specObject := range specArr {
-		if err := ApplySpec(ctx, token, model.GatewayAddr, specObject); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// ApplySpec takes a spec object and applies it
-func ApplySpec(ctx context.Context, token, hostAddr string, specObj *model.SpecObject) error {
-	requestBody, err := json.Marshal(specObj.Spec)
-	if err != nil {
-		return helpers.Logger.LogError(helpers.GetRequestID(ctx), "error while applying service unable to marshal spec", err, nil)
-	}
-	url, err := adjustPath(fmt.Sprintf("%s%s", hostAddr, specObj.API), specObj.Meta)
-	if err != nil {
+	if err := MakeHTTPRequest(token, http.MethodPost, fmt.Sprintf("%s/v1/config/batch-apply", model.GatewayAddr), map[string]string{}, map[string]interface{}{"specs": specArr}, new(map[string]interface{})); err != nil {
 		return err
 	}
-
-	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(requestBody))
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return helpers.Logger.LogError(helpers.GetRequestID(ctx), "error while applying service unable to send http request", err, nil)
-	}
-
-	v := map[string]interface{}{}
-	_ = json.NewDecoder(resp.Body).Decode(&v)
-	CloseTheCloser(req.Body)
-
-	if resp.StatusCode == http.StatusAccepted {
-		// Make checker send this status
-		helpers.Logger.LogInfo(helpers.GetRequestID(ctx), fmt.Sprintf("Successfully queued %s", specObj.Type), nil)
-	} else if resp.StatusCode == http.StatusOK {
-		helpers.Logger.LogInfo(helpers.GetRequestID(ctx), fmt.Sprintf("Successfully applied %s", specObj.Type), nil)
-	} else {
-		_ = helpers.Logger.LogError(helpers.GetRequestID(ctx), fmt.Sprintf("error while applying service got http status code %s", resp.Status), fmt.Errorf("%s", v["error"]), nil)
-		return fmt.Errorf("%v", v["error"])
-	}
 	return nil
-}
-
-func adjustPath(path string, meta map[string]string) (string, error) {
-	newPath := path
-	for {
-		pre := strings.IndexRune(newPath, '{')
-		if pre < 0 {
-			return newPath, nil
-		}
-		post := strings.IndexRune(newPath, '}')
-
-		key := strings.TrimSuffix(strings.TrimPrefix(newPath[pre:post], "{"), "}")
-		value, p := meta[key]
-		if !p {
-			return "", fmt.Errorf("provided key (%s) does not exist in metadata", key)
-		}
-
-		newPath = newPath[:pre] + value + newPath[post+1:]
-	}
 }
 
 // GetDbRule gets database rule
@@ -131,7 +70,7 @@ func GetDbRule(token, project, commandName string, params map[string]string) ([]
 	url := fmt.Sprintf("/v1/config/projects/%s/database/collections/rules", project)
 	// Get the spec from the server
 	payload := new(model.Response)
-	if err := MakeHTTPRequest(token, http.MethodGet, url, params, payload); err != nil {
+	if err := MakeHTTPRequest(token, http.MethodGet, url, params, new(map[string]interface{}), payload); err != nil {
 		return nil, err
 	}
 
@@ -163,7 +102,7 @@ func GetDbConfig(token, project, commandName string, params map[string]string) (
 	url := fmt.Sprintf("/v1/config/projects/%s/database/config", project)
 	// Get the spec from the server
 	payload := new(model.Response)
-	if err := MakeHTTPRequest(token, http.MethodGet, url, params, payload); err != nil {
+	if err := MakeHTTPRequest(token, http.MethodGet, url, params, new(map[string]interface{}), payload); err != nil {
 		return nil, err
 	}
 
@@ -195,7 +134,7 @@ func GetDbSchema(token, project, commandName string, params map[string]string) (
 
 	// Get the spec from the server
 	payload := new(model.Response)
-	if err := MakeHTTPRequest(token, http.MethodGet, url, params, payload); err != nil {
+	if err := MakeHTTPRequest(token, http.MethodGet, url, params, new(map[string]interface{}), payload); err != nil {
 		return nil, err
 	}
 
@@ -224,7 +163,7 @@ func GetDbPreparedQuery(token, project, commandName string, params map[string]st
 	url := fmt.Sprintf("/v1/config/projects/%s/database/prepared-queries", project)
 
 	payload := new(model.Response)
-	if err := MakeHTTPRequest(token, http.MethodGet, url, params, payload); err != nil {
+	if err := MakeHTTPRequest(token, http.MethodGet, url, params, new(map[string]interface{}), payload); err != nil {
 		return nil, err
 	}
 
